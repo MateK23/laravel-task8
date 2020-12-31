afb8d7f188bffed4690736946f64733fee7fed42
MateK23 <32340202+MateK23@users.noreply.github.com>
2020-12-31 14:21:54 +0400

    3rd

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -22,5 +23,16 @@ class NewsController extends Controller
             'user_id' => 'required|unique:news|max255',
             'category_id' => 'nullable'
         ]);
+        foreach ($validatedData as $data)
+            News::create([
+                'title' => $data->input('title'),
+                'description' => $data->input('description'),
+                'short_description' => $data->input('short_description'),
+                'image' => $data->input('image'),
+                'upload_date' => $data->now()->toDateTimeString(),
+                'user_id' => $data->Auth::user()->id,
+                'category_id' => $data->input('category_id'),
+            ]);
+        return redirect('/home');
     }
 }
