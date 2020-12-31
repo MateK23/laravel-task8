91809fb68b940179bd1d175228c96a5210646c52
MateK23 <32340202+MateK23@users.noreply.github.com>
2020-12-31 13:50:07 +0400

    first

---
+++ database/migrations/2020_12_31_094047_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('short_description');
+            $table->string('image');
+            $table->string('upload_date');
+            $table->timestamps();
+            $table->bigInteger('user_id');
+            $table->bigInteger('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
