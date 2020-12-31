200f35f8c2e1a927eb30bd09b8d790a13321ebc5
MateK23 <32340202+MateK23@users.noreply.github.com>
2020-12-31 14:06:08 +0400

    2nd

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|unique:news|max:255',
+            'description' => 'required',
+            'short_description' => 'required',
+            'image' => 'nullable',
+            'upload_date' => 'required|date',
+            'user_id' => 'required|unique:news|max255',
+            'category_id' => 'nullable'
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('news/create', 'NewsController@create');
+Route::post('news', 'NewsController@store');
